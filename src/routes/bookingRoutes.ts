95f113c7f2d7b252dd3f128d251ef91e81d6abcd
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-09-19 07:17:02 +0200

    Ændringer for deployment til render

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { Authorize } from '../middleware/authMiddleware';
 import { createRecord, deleteRecord, getRecord, getRecords, getRecordsByUserId } from '../controllers/bookingController';
+import catchAsync from '../utils/catchAsync';
 
 const routes = Router();
 routes.get('/', getRecords);
 routes.get('/byId/:id', getRecord);
-routes.get('/byUser', Authorize, getRecordsByUserId);
-routes.post('/', Authorize, createRecord);
-routes.delete('/:id', Authorize, deleteRecord);
+routes.get('/byUser', catchAsync(Authorize), catchAsync(getRecordsByUserId));
+routes.post('/', catchAsync(Authorize), catchAsync(createRecord));
+routes.delete('/:id', catchAsync(Authorize), catchAsync(deleteRecord));
 
 export const bookingRoutes = routes;
